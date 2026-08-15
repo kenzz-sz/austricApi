fd1d76594bdffc531ca6d87434a80dcdf4a4484c
kenzz-sz <r4mseiandsei@gmail.com>
2026-08-15 15:45:23 +0700

    update login.js

--- api/login.js
+++ api/login.js
@@ -1,16 +1,4 @@
-
 const admin = require('firebase-admin');
-
-if (!admin.apps.length) {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://austric-a99e0-default-rtdb.asia-southeast1.firebasedatabase.app"
-    });
-}
-
-const db = admin.database();
 
 module.exports = async function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,13 +9,26 @@ module.exports = async function(req, res) {
         return res.status(200).end();
     }
 
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method Not Allowed' });
-    }
-
     try {
-        const { username, password } = req.body;
+        if (!admin.apps.length) {
+            if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+                throw new Error("Kunci Rahasia Firebase belum terpasang di Vercel!");
+            }
+            
+            const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+                databaseURL: "https://austric-a99e0-default-rtdb.asia-southeast1.firebasedatabase.app"
+            });
+        }
 
+        const db = admin.database();
+
+        if (req.method !== 'POST') {
+            return res.status(405).json({ error: 'Method Not Allowed' });
+        }
+
+        const { username, password } = req.body;
         const usersRef = db.ref('data/account');
         const userQuery = usersRef.orderByChild('username').equalTo(username);
         
@@ -42,16 +43,16 @@ module.exports = async function(req, res) {
         const user = data[userKey];
 
         if (user.password === password) {
-            return res.status(200).json({ 
-                success: true,
-                userData: user 
-            });
+            return res.status(200).json({ success: true, userData: user });
         } else {
             return res.status(401).json({ success: false, invalidCode: 1 });
         }
 
     } catch (error) {
-        console.error("Error Detail:", error);
-        return res.status(500).json({ error: "Terjadi kesalahan pada server." });
+        console.error("Server Error:", error.message);
+        return res.status(500).json({ 
+            error: "Internal Server Error", 
+            detail: error.message 
+        });
     }
 };
